eee0c1acc2659220e18dbdbb0d25a2a54657f56f
Lucas Moura Gonçalves <92659220+LucasMZ@users.noreply.github.com>
2023-05-30 17:19:13 -0300

    1.1

--- file.js
+++ file.js
@@ -1,50 +1,45 @@
-let src = ["facebook.png",
-            "apple.png",
-            "paypal.png",
-            "twitter.png"]
-let width = [75,75,75,75,75];
-let height = [70.70,70,70,70];
-let botao = document.querySelector(".button");
-let click = 0;
-botao.addEventListener('click', function(){
-  if(click < 3){
-  display_image(src,width,height);
-  click++;
+
+
+
+class ImageDisplay {
+  constructor(src, width, height) {
+    this.src = src;
+    this.width = width;
+    this.height = height;
+    this.click = 0;
+    this.button = document.querySelector(".button");
+    this.button.addEventListener('click', this.handleClick.bind(this));
   }
-});
-function display_image(src,width,height) {
-  var a = [];
-    var a = document.createElement("img");
-    let i = Math.floor(Math.random()*src.length);
-    
-    a.src = src[i];
-    a.width = width[i];
-    a.height = height[i]; 
-    let y = []
-     y = document.querySelectorAll(".box-1, .box-2");
-     y.forEach(function(element) {
-      let i = Math.floor(Math.random()*src.length);
-      a.src = src[i];
-      element.appendChild(a.cloneNode(true));
-    });
-    y = document.querySelectorAll(".box-3, .box-4");
-    y.forEach(function(element) {
-      let j = Math.floor(Math.random()*src.length);
-      a.src = src[j];
-     element.appendChild(a.cloneNode(true));
-   });
-   y = document.querySelectorAll(".box-5, .box-6");
-    y.forEach(function(element) {
-      let j = Math.floor(Math.random()*src.length);
-      a.src = src[j];
-     element.appendChild(a.cloneNode(true));
-   });
-   y = document.querySelectorAll(".box-7, .box-8, .box-9");
-   y.forEach(function(element) {
-    let i = Math.floor(Math.random()*src.length);
-    a.src = src[i];
-    element.appendChild(a.cloneNode(true));
-  });
+  
+  handleClick() {
+    if (this.click < 1) {
+      this.displayImage();
+      this.click++;
     }
   
- 
+    setTimeout(() => {
+      window.location.reload(true);
+    }, 3000);
+  }
+  
+  displayImage() {
+    let images = document.querySelectorAll(".box-1, .box-2, .box-3, .box-4, .box-5, .box-6, .box-7, .box-8, .box-9");
+    
+    images.forEach((element) => {
+      let i = Math.floor(Math.random() * this.src.length);
+      let image = document.createElement("img");
+      image.src = this.src[i];
+      image.width = this.width[i];
+      image.height = this.height[i];
+      element.appendChild(image);
+    
+    });
+  }
+}
+
+let src = ["facebook.png", "apple.png", "paypal.png", "twitter.png"];
+let width = [75, 75, 75, 75, 75];
+let height = [70.70, 70, 70, 70];
+
+let imageDisplay = new ImageDisplay(src, width, height);
+
